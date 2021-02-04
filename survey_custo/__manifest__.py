020015951e519857f52732c5f36f8094bae2ddf1
AntoineSadzot <Sadzot.Antoine@gmail.com>
2021-02-04 15:23:41 +0100

    Scaffold

---
+++ survey_custo/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Survey custo",
+
+    'description': """
+        Curstomize the survey module
+    """,
+
+    'author': "asd, mar, sua, aup",
+    'website': "http://www.odoo.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['survey'],
+
+    'data': [
+    ],
+}
---
+++ survey_custo/models/account.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import RedirectWarning, UserError, ValidationError, AccessError
+
+class AccountPayment(models.Model):
+    _inherit="account.move"
+
+    def button_draft(self):
+        AccountMoveLine = self.env['account.move.line']
+        excluded_move_ids = []
+
+        if self._context.get('suspense_moves_mode'):
+            excluded_move_ids = AccountMoveLine.search(AccountMoveLine._get_suspense_moves_domain() + [('move_id', 'in', self.ids)]).mapped('move_id').ids
+
+        for move in self:
+            if move in move.line_ids.mapped('full_reconcile_id.exchange_move_id'):
+                raise UserError(_('You cannot reset to draft an exchange difference journal entry.'))
+            if move.tax_cash_basis_rec_id:
+                raise UserError(_('You cannot reset to draft a tax cash basis journal entry.'))
+            if move.restrict_mode_hash_table and move.state == 'posted' and move.id not in excluded_move_ids:
+                raise UserError(_('You cannot modify a posted entry of this journal because it is in strict mode.'))
+            # We remove all the analytics entries for this journal
+            move.mapped('line_ids.analytic_line_ids').unlink()
+
+        #-----removed instruction concerning deleting reconciliation-----#
+        self.write({'state': 'draft'})
+
+
+        
